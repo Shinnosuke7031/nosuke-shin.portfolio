5284f686717d21b6762db64919d8e5caa15a7c3f
Shinnosuke Iino <sengoku731sin@gmail.com>
2021-12-19 22:49:00 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: process.env.GITHUB_ACTIONS ? "/nosuke-shin.portfolio" : "",
+  trailingSlash: true,
+};
